5a3ce12c641a69de488596eaa8dc002e0e11af46
Ben Imberman <bimberman@gmail.com>
2025-03-11 13:17:33 -0700

    completed

---
+++ completed-exercises/typescript-operators-and-expressions/main.ts
@@ -0,0 +1,38 @@
+const width: number = 10;
+const height: number = 50;
+const area: number = width * height;
+console.log(`value of area: ${area}`);
+console.log(`typeof area: ${typeof area}`);
+
+const bill: number = 20;
+const payment: number = 30;
+const change: number = payment - bill;
+console.log(`value of change: ${change}`);
+console.log(`typeof change: ${typeof change}`);
+
+const quizzes: number = 5;
+const midterm: number = 10;
+const final: number = 20;
+const grade: number = (quizzes + midterm + final) / 3;
+console.log(`value of grade: ${grade}`);
+console.log(`typeof grade: ${typeof grade}`);
+
+const firstName: string = 'Ben';
+const lastName: string = 'Imberman';
+const fullName: string = `${firstName} ${lastName}`;
+console.log(`value of fullName: ${fullName}`);
+console.log(`typeof fullName: ${typeof fullName}`);
+
+const pH: number = 8;
+const isAcidic: boolean = pH < 7;
+console.log(`value of isAcidic: ${isAcidic}`);
+console.log(`typeof isAcidic: ${typeof isAcidic}`);
+
+const headCount: number = 300;
+const isSparta: boolean = headCount === 300;
+console.log(`value of isSparta: ${isSparta}`);
+console.log(`typeof isSparta: ${typeof isSparta}`);
+
+const motto: string = fullName + ' is the GOAT';
+console.log(`value of motto: ${motto}`);
+console.log(`typeof motto: ${typeof motto}`);
---
+++ completed-exercises/typescript-operators-and-expressions/typescript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,48 @@
+# javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What data type is returned by an arithmetic operation?<br/>
+  A single numerical value.<br/>
+
+- What is string concatenation?<br/>
+  Joins two or more string values.<br/>
+
+- What purpose(s) does the `+` plus operator serve in JavaScript?<br/>
+  The addition operator produces the sum of numeric operands or string concatenation<br/>
+
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?<br/>
+  Boolean.<br/>
+
+- What does the `+=` "plus-equals" operator do?<br/>
+  The addition assignment (+=) operator performs addition (which is either numeric addition or string concatenation) on the two operands and assigns the result to the left operand. <br/>
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
