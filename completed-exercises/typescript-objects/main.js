20b5c2e6c5a3511c97633d00d4b93ff71cd6ba24
Ben Imberman <bimberman@gmail.com>
2025-03-11 16:22:20 -0700

    completed

--- completed-exercises/typescript-objects/main.js
+++ completed-exercises/typescript-objects/main.js
@@ -1,59 +1,32 @@
-interface StudentProps {
-  firstName: string;
-  lastName: string;
-  age: number;
-  livesInIrvine?: boolean;
-  previousOccupation?: string;
-}
-
-const student: StudentProps = {
+'use strict';
+const student = {
   firstName: 'Mark',
   lastName: 'Grayson',
   age: 19,
 };
-
-const fullName: string = student.firstName + ' ' + student.lastName;
+const fullName = student.firstName + ' ' + student.lastName;
 console.log('value of fullName:', fullName);
-
 student.livesInIrvine = true;
 student.previousOccupation = 'Software Engineer';
-
 console.log('value of student.livesInIrvine:', student.livesInIrvine);
 console.log('value of student.previousOccupation:', student.previousOccupation);
 console.log('value of student:', student);
 console.log('typeof of student:', typeof student);
-
-interface Vehicle {
-  make: string;
-  model: string;
-  year: number;
-  color?: string;
-  isConvertible?: boolean;
-}
-
-const vehicle: Vehicle = {
+const vehicle = {
   make: 'Toyota',
   model: 'Rav4',
   year: 2024,
 };
-
 vehicle['color'] = 'blue';
 vehicle['isConvertible'] = false;
 console.log("value of vehicle['color']:", vehicle['color']);
 console.log("value of vehicle['isConvertible']:", vehicle['isConvertible']);
 console.log('value of vehicle:', vehicle);
 console.log('typeof of vehicle:', typeof vehicle);
-
-interface Pet {
-  name?: string;
-  kind?: string;
-}
-
-const pet: Pet = {
+const pet = {
   name: 'Luna',
   kind: 'Dog',
 };
-
 delete pet.name;
 delete pet.kind;
 console.log('value of pet:', pet);
